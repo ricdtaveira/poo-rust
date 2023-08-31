d69de112a9940c5e419f7ba208737503ddf02392
Ricardo Taveira <ridtaveira@gmail.com>
2023-08-31 10:54:29 -0300

    Alterado em 31-08-2023

---
+++ 01-classes/poo_classe/target/debug/deps/poo_classe.d
@@ -0,0 +1,5 @@
+E:\00-Rust\poo-rust\01-classes\poo_classe\target\debug\deps\poo_classe.exe: src\main.rs
+
+E:\00-Rust\poo-rust\01-classes\poo_classe\target\debug\deps\poo_classe.d: src\main.rs
+
+src\main.rs:
---
+++ 01-classes/poo_classe/target/debug/poo_classe.d
@@ -0,0 +1,1 @@
+E:\00-Rust\poo-rust\01-classes\poo_classe\target\debug\poo_classe.exe: E:\00-Rust\poo-rust\01-classes\poo_classe\src\main.rs
